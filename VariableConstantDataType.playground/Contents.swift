e4634f3179042f3484bdcf6f1f0f28343c43f2dd
Archer <Archer@ArcherdeMacBook-Pro.local>
2015-05-14 17:05:00 +0800

    Add New File

---
+++ VariableConstantDataType.playground/Contents.swift
@@ -0,0 +1,58 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*******************************************************
+*
+*   [ Swift ] 變數、常數、資料型態
+*
+*******************************************************/
+
+let age:Int = 23
+var title:String = "Hello", name:String
+name = "Archer"
+
+print("\(title) \(name), age: \(age) ")
+
+var heigth = 177.5, weight = 60
+var total = heigth + Double(weight)
+
+print("heigth: \(heigth), weight: \(weight), total: \(total) ")
+
+var isMale:Bool = true
+
+print("isMale: \(isMale)")
+
+/*******************************************************
+*
+*   常數 (Constant)
+*
+*******************************************************/
+
+let constant = 100
+
+print("\(constant)")
+
+/*******************************************************
+*
+*   變數 (Variable)
+*
+*******************************************************/
+
+var variable = 200
+
+print("\(variable)")
+
+/*******************************************************
+*
+*   資料型態
+*
+*******************************************************/
+
+var variable1:Int = 300
+
+print("\(variable1)")
+
+var variable2:Int = 5_000_000
+
+print("\(variable2)")
